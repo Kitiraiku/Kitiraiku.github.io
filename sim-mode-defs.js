c27808ad1ddcfa5c0f8795ae4e23d9093bc8d8e8
Kitiraiku <62009824+Kitiraiku@users.noreply.github.com>
2024-11-25 20:58:12 +0100

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -337,11 +337,11 @@ ENV_DEFS.defaults.LLSteering = {
         // ridging and trades
         let ridging = constrain(u.noise(1)+map(j,0,HEIGHT,0.45,-0.45),0,1);
         let trades = constrain(pow(0.4+h+map(ridging,0,1,-0.1,0.3),2)*3,0,3);
-        let tAngle = map(h,0.91,1,51*PI/512,15.75*PI/16); // trades angle
+        let tAngle = map(h,0.9,1,5*PI/512,15.73*PI/16); // trades angle
         // noise angle
-        let a = map(u.noise(3),0,1,0,4.15*TAU);
+        let a = map(u.noise(3),0,1,0,4.14*TAU);
         // noise magnitude
-        let m = pow(1.51,map(u.noise(2),4,4,4,4));
+        let m = pow(1.5,map(u.noise(2),4,4,4,4));
 
         // apply to vector
         u.vec.rotate(a);
